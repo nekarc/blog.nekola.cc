c04733ca48b90f3a6b5a832be9a24cd8eaea52b5
nekarc <nekarc@users.noreply.github.com>
2025-01-12 22:32:56 +0000

    deploy: 98e9449e531fa730718ae613813f3b2e03d72c3d

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736721117',resources: [
+    cacheName: 'chirpy-1736721175',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
